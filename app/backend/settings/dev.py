ea09d0b52f973704ae97a11e024d5261882da28e
Maham <mhm.anwaar2@gmail.com>
2023-04-11 23:10:15 +0500

    auth is now handled by email

--- app/backend/settings/dev.py
+++ app/backend/settings/dev.py
@@ -1,6 +1,7 @@
 import os
 
 from .default import *
+from datetime import timedelta
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
@@ -9,7 +10,7 @@ DEBUG = int(os.environ.get("DEBUG", default=0))
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
 ALLOWED_HOSTS = ['*'] 
-CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split(" ")
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000', 'http://localhost:8000', 'http://0.0.0.0:8000']
 
 INSTALLED_APPS += [
     # third party
@@ -44,11 +45,35 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": ["rest_framework_simplejwt.authentication.JWTAuthentication"],
-    "PAGE_SIZE": 15,
+    # "PAGE_SIZE": 15,
 }
-AUTHENTICATION_BACKENDS = ['users.auth_backends.EmailBackend']
-
-del AUTH_PASSWORD_VALIDATORS[-3:]
-AUTH_PASSWORD_VALIDATORS.append({"NAME": "db_data.validations.PasswordValidation"})
 
 AUTH_USER_MODEL = "user.User"
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "UPDATE_LAST_LOGIN": True,
+    "ALGORITHM": "HS256",
+    "SIGNING_KEY": SECRET_KEY,
+    "VERIFYING_KEY": None,
+    "AUDIENCE": None,
+    "ISSUER": None,
+    "JWK_URL": None,
+    "LEEWAY": 0,
+    "AUTH_HEADER_TYPES": ("Token",),
+    "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",
+    "USER_AUTHENTICATION_RULE": (
+        "rest_framework_simplejwt.authentication.default_user_authentication_rule"
+    ),
+    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+    "TOKEN_TYPE_CLAIM": "token_type",
+    "JTI_CLAIM": "jti",
+    "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
+    "SLIDING_TOKEN_LIFETIME": timedelta(minutes=10),
+    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=30),
+}
--- app/user/models.py
+++ app/user/models.py
@@ -1,6 +1,37 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.db import models
 
-class User(AbstractUser):
+class UserManager(BaseUserManager):
+    
+    def create_account(self, email, password=None, **extra_fields):
+        """Creates & saves a new user."""
+        if not email:
+            raise ValueError("Users must have an email address!")
+        account = self.model(email=self.normalize_email(email), **extra_fields)
+        account.set_password(password)
+        account.save(using=self._db)
+        return account
+
+    def create_superuser(self, email, password, **kwargs):
+        """Create and saves a new super user."""
+        account = self.create_account(email, password, **kwargs)
+        account.is_active = True
+        account.is_staff = True
+        account.is_superuser = True
+        account.save(using=self._db)
+        return account
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(blank=True, unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    is_staff = models.BooleanField(default=False)
+
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
     
     @property
     def return_full_name(self):
